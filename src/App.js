35569ce82c409333c331e826a1bcd1e2ddd4e44a
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2022-02-13 01:16:27 +0100

    what the fuuu?

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import { Add } from './Components/Add';
 import './Styles/Style.css'
 function App() {
   const [isAdd,setIsAdd] = useState(false)
-  const [isAddTxt,setIsAddTxt] = useState(false)
-  const addChanger = (e) => {
-    e.preventDefault()
-    setIsAdd(!isAdd)
-    setIsAddTxt(!isAddTxt)
-  }
   return (
     <div className="App">
       <div className="mainContainer">
@@ -17,8 +11,8 @@ function App() {
           <h1>Task Tracker</h1>
           <div style={isAdd? {backgroundColor : 'red'}: {backgroundColor:'green'}} 
           className="addClose" 
-          onClick={addChanger} >
-            {!isAddTxt ? 'Add':'Close'}</div>
+          onClick={()=> setIsAdd(!isAdd)} >
+            {!isAdd ? 'Add':'Close'}</div>
         </div>
         {isAdd && <Add/>}
         <Tasks/>
